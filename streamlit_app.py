52648916cf98cbd02341b8ff743bb2d3266b1831
nithudesai <131021804+nithudesai@users.noreply.github.com>
2024-09-10 22:24:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,28 @@
-import streamlit as st
+def radio_change(label):
+    if label == 'Reject':
+        st.text_area(label='Reject Reason')
+        st.text_input('Keyword search:', '')
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+def search_dropdown(search_keyword):
+    if search_keyword:
+        # Filtering logic
+        filtered_df = 'test'
+
+        # Display filtered results in a dropdown
+        selected_item = st.selectbox('Select a suggestion:', ['Opt 1', 'Opt 2', 'Opt 3', ])
+
+        st.write(f'Selected item: **{selected_item}**')
+
+
+label_options = ['Accept', 'Reject']
+selected_label = st.radio("Select Label:", label_options, key='label_radio')
+
+with st.form('test'):
+    if selected_label == 'Reject':
+        reject_text = st.text_area(label='Reject Reason')
+        search_keyword = st.text_input('Keyword search:', '')
+
+        # Displays a dropdown populated with search results from search_keyword
+        search_dropdown(search_keyword)
+    submit_button = st.form_submit_button('Submit')
